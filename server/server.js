521f8ebf1937c586fa8f9b0f17eed56a3d5ba517
Your Name <you@example.com>
2024-01-23 01:20:50 +0100

    fist commit

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your Mongo Atlas URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb+srv://fedronicfelix:yPL2GphDWjYRoPFB@test-graphql.r4nz8e5.mongodb.net/?retryWrites=true&w=majority';
 if (!MONGO_URI) {
   throw new Error('You must provide a Mongo Atlas URI');
 }
