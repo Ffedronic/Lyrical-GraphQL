aed8370968c711e4541cf0c0ba71de68b848f054
Your Name <you@example.com>
2024-01-24 14:37:35 +0100

    query songs from mongoDB

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,11 +1,48 @@
-import React, { Component } from 'react'
-
+import React, { Component } from "react";
+import gql from "graphql-tag";
+import { graphql } from "react-apollo";
 export class SongList extends Component {
+  renderSongs(songs) {
+    return songs.map((song) => (
+      <li key={song.id}>
+        <ul>
+          <li>{song.title}</li>
+          <ul>
+            {song.lyrics.map((lyric, index) => (
+              <li key={index}>{lyric.content}</li>
+            ))}
+          </ul>
+        </ul>
+      </li>
+    ));
+  }
+
   render() {
-    return (
-      <div>SongList</div>
-    )
+    const { error, loading, songs } = this.props.data;
+
+    if (loading) {
+      return <div>Loading...</div>;
+    }
+
+    if (error) {
+      console.log(error);
+      return <div>Something goes wrong. Try later...</div>;
+    }
+
+    return <ul>{this.renderSongs(songs)}</ul>;
   }
 }
 
-export default SongList
+const query = gql`
+  {
+    songs {
+      id
+      title
+      lyrics {
+        content
+      }
+    }
+  }
+`;
+
+export default graphql(query)(SongList);
