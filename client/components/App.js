bd384cff4ca3aab94e95ccf49ddb1f62b04b05bf
Your Name <you@example.com>
2024-01-24 15:15:08 +0100

    app

--- client/components/App.js
+++ client/components/App.js
@@ -3,7 +3,7 @@ import {Link} from "react-router"
 export class App extends Component {
   render() {
     return (
-      <div>
+      <div className="container">
         <h1>Lyrics App</h1>
         <ul>
             <li><Link to="/">Home</Link></li>
